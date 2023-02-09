396f6f00644ded553fa72d70f133684a95e07c36
JFladsonSIlva <fladsonsilva1@gmail.com>
2023-02-09 20:20:04 -0300

    req 3 OK

---
+++ src/pages/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Cart extends React.Component {
+  render() {
+    return (
+      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+    );
+  }
+}
+
+export default Cart;
--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class ProductList extends React.Component {
   render() {
     return (
-      <p
-        data-testid="home-initial-message"
-      >
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+      <>
+        <p
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <Link data-testid="shopping-cart-button" to="/cart" />
+      </>
     );
   }
 }
