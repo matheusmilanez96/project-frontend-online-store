47e3c41e4467e16be22362710ac85ac1fbfd461c
Matheus Milanez <mattmila96@gmail.com>
2023-02-09 23:33:20 -0300

    requisito 4

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 
 class ProductList extends React.Component {
+  state = {
+    categoriesList: [],
+  };
+
+  async componentDidMount() {
+    const categories = await getCategories();
+    console.log(categories);
+    this.setState({
+      categoriesList: categories,
+    });
+  }
+
   render() {
+    const {
+      categoriesList,
+    } = this.state;
     return (
-      <p
-        data-testid="home-initial-message"
-      >
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+      <div>
+        <p
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <ul>
+          { categoriesList.map((cat) => (
+            <button data-testid="category" key={ cat.id }>{ cat.name }</button>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
